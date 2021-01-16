363abc61b6b0deba5acce0829c3ebc514f33aa6b
matheusosp <matheusosp@users.noreply.github.com>
2021-01-16 20:31:18 -0300

    added template services with database

--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from django.views.generic import TemplateView
+from .models import Services, Employee
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-class View404(TemplateView):
-    template_name = '404.html'
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Services.objects.order_by('?').all()
+        context['employees'] = Employee.objects.all()
+        return context
