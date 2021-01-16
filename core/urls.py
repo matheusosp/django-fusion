dcc9774346d8355f1faed32af81f8a718b991a66
matheusosp <matheusosp@users.noreply.github.com>
2021-01-16 18:46:25 -0300

    404 and 500 error pages added

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, View404
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', IndexView.as_view(), name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+class View404(TemplateView):
+    template_name = '404.html'
